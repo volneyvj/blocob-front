26a5e6d69c3d9e37a21f9ee37f277357c8eb5ae5
volneyvj <volney.faustini@gmail.com>
2021-04-24 11:39:09 -0300

    deployed first

--- src/utils/api.util.js
+++ src/utils/api.util.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Api {
   constructor() {
     this.api = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: "https://api-blocob.herokuapp.com/",
     });
 
     this.api.interceptors.request.use(
