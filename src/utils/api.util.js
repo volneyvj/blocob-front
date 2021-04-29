afaa4007bb4c88f7ab3ccf3e13af70140e13cd1f
volneyvj <volney.faustini@gmail.com>
2021-04-29 19:45:38 -0300

    web server

--- src/utils/api.util.js
+++ src/utils/api.util.js
@@ -3,8 +3,8 @@ import axios from "axios";
 class Api {
   constructor() {
     this.api = axios.create({
-    // baseURL: "https://api-blocob.herokuapp.com/",
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://api-blocob.herokuapp.com/",
+    // baseURL: "http://localhost:5000/",
     });
 
     this.api.interceptors.request.use(
